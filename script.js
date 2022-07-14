eac56c73fe97426df29012636db0c4aea16b9eac
DanGraziano <dangraziano@outlook.com>
2022-07-14 14:13:55 -0400

    Add operate function

--- script.js
+++ script.js
@@ -33,14 +33,20 @@ console.log(divide(6, 2))
 // Create a new function operate that takes an operator and 2 numbers 
 // and then calls one of the above functions on the numbers.
 
-/*
-const operate = function(operation) {
-    return add(a, b)
+function operate(num1, num2, operator) {
+    switch (operator) {
+      case "+":
+        return add(num1, num2);
+      case "-":
+        return subtract(num1, num2);
+      case "*":
+        return multiply(num1, num2);
+      case "/":
+        return divide(num1, num2);
+    }
+  };
 
-}
-
-console.log(operate())
-*/
+  console.log(operate(1, 4, "*"))
 
 // Number button clicking
 
