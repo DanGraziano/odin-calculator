7f3e3fd1fdd6de56cf1680aa9cd30713955ec5a2
DanGraziano <dangraziano@outlook.com>
2022-07-16 21:50:24 -0400

    Add event listeners for number and operators button clicks

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ const displayValue = document.querySelector('.display');
 const deleteButton = document.querySelector('.delete')
 const equalsButton = document.querySelector('.equals');
 
-
-
 // Sum of a,b
 
 const add = function(a, b)  {
@@ -68,3 +66,71 @@ function operate(a, b, operator) {
 
 // Create the functions that populate the display when you click the number buttons. 
 // You should be storing the ‘display value’ in a variable somewhere for use in the next step.
+
+// Need the numbers to appear in the output. 
+// Maybe change innerHTML? Based on the button click? 
+// But then how do we keep from changing each time for when we need 
+// multiple clicks like the number 50 or 100?
+
+allClearButton.addEventListener('click', function() {
+    console.log("All clear");
+    location.reload()
+});
+
+deleteButton.addEventListener('click', function() {
+    console.log("Delete");
+    // Needs ability to delete previous number only
+});
+
+equalsButton.addEventListener('click', function() {
+    console.log("=");
+    a = parseFloat(a);
+    b = parseFloat(b);
+    operate(a, b, operator);
+    // Needs ability to have a = first button press and b = second one after operator
+
+})
+
+ // Number button click that returns the number
+
+ numberButtons.forEach(numberButtons => {
+    numberButtons.addEventListener('click', () => {
+        displayValue.innerHTML = numberButtons.value
+        console.log(numberButtons.value);
+    });
+ });
+
+// Operator button click return the operator
+
+operatorButtons.forEach(operatorButtons => {
+    operatorButtons.addEventListener('click', () => {
+        displayValue.textContent = operatorButtons.value
+        console.log(operatorButtons.value);
+    });
+ });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /*
+for (let i = 0; i < numberButtons.length; i++) {
+    numberButtons[i].addEventListener("click", function () {
+        console.log(this.innerHTML)
+    });
+}
+*/
